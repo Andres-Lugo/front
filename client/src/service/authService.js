707b2b40149d61336e43eb465218237fd7954144
Andres Lugo <pipeandres191@gmail.com>
2025-09-09 02:36:03 -0500

    fix: debug registro

--- client/src/service/authService.js
+++ client/src/service/authService.js
@@ -1,16 +1,13 @@
-// /client/src/services/authService.js
 import axios from 'axios';
 
-const API_URL = `${import.meta.env.VITE_API_BASE_URL}/auth`;
+const API_URL = `${process.env.REACT_APP_API_BASE_URL}/auth`;
 
 export const login = async (email, password) => {
     const res = await axios.post(`${API_URL}/login`, { email, password });
     localStorage.setItem('token', res.data.token);
-    return res.data; // 👈 devolvemos la respuesta del backend
 };
 
 export const register = async (email, password) => {
     const res = await axios.post(`${API_URL}/register`, { email, password });
     localStorage.setItem('token', res.data.token);
-    return res.data; // 👈 devolvemos la respuesta del backend
 };
