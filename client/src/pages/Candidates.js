e778994e3c3417dd4575f09876c055e326a78c77
Andres Lugo <pipeandres191@gmail.com>
2025-09-18 18:37:03 -0500

    estilos

--- client/src/pages/Candidates.js
+++ client/src/pages/Candidates.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import './candidates.css';
+
 
 function Candidates() {
   const [candidates, setCandidates] = useState([]);
